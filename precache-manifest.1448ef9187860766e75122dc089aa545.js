493ae832893cf0e61219caa8dcf224fdc1ac057a
rain-js <unique_ritchie@163.com>
2019-08-20 16:42:35 +0800

    Updates

---
+++ precache-manifest.1448ef9187860766e75122dc089aa545.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4801779180eee7918c3747047190f524",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "849968dbc8ec86f431d2",
+    "url": "/react-tutorial/static/css/main.682ca21c.chunk.css"
+  },
+  {
+    "revision": "854d68ec4190dad53f1b",
+    "url": "/react-tutorial/static/js/2.d7b9399b.chunk.js"
+  },
+  {
+    "revision": "849968dbc8ec86f431d2",
+    "url": "/react-tutorial/static/js/main.4dc4e3c2.chunk.js"
+  },
+  {
+    "revision": "be44a969ea6f00258bce",
+    "url": "/react-tutorial/static/js/runtime~main.4adc765e.js"
+  }
+]);
